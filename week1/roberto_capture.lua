c1efac2cadfc5838a28b937380dbdfa1f47826fb
Matt Wilbur <wilburm@gmail.com>
2023-08-20 08:26:17 -0400

    update

--- week1/roberto_capture.lua
+++ week1/roberto_capture.lua
@@ -5,8 +5,9 @@ function add(a,b)
 end
 
 local space = lpeg.S(" \n\t")^0
-local numeral = lpeg.R("09")^1 / tonumber * space
+local numeral = (lpeg.P("-")^-1 * lpeg.R("09")^1) / tonumber * space
 local op = lpeg.P("+") * space
 local sum = (numeral * op * numeral) / add
 
 print(sum:match("12 + 20"))
+print(sum:match("12 + -20"))
