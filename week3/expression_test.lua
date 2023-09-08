3e7f67f0fe3e8e3bcccd3a2456ffa04897ac0238
Matt Wilbur <wilburm@gmail.com>
2023-09-08 11:29:06 -0400

    whitespace fixing
    whitespace fixiing
    
    whitespace fixiing

--- week3/expression_test.lua
+++ week3/expression_test.lua
@@ -1,35 +1,35 @@
 do
-	local expr = require "expression"
-	local pt = require "pt"
+    local expr = require "expression"
+    local pt = require "pt"
 
-	local parse,compile,run = expr.parse, expr.compile, expr.run
+    local parse,compile,run = expr.parse, expr.compile, expr.run
 
-	local input = io.read("*a")
-	--local input = "a =  1 "
-	local ast = parse(input)
-	print(pt.pt(ast))
+    local input = io.read("*a")
+    --local input = "a =  1 "
+    local ast = parse(input)
+    print(pt.pt(ast))
     ---[[
-	local code = compile(ast)
-	print(pt.pt(code))
-	--store = {k1 = 1, k2 = 2, k10 = 10, __q=15, ["k'"]=32}
+    local code = compile(ast)
+    print(pt.pt(code))
+    --store = {k1 = 1, k2 = 2, k10 = 10, __q=15, ["k'"]=32}
     store = {}
-	stack = {}
+    stack = {}
     vmio = {
         out = function(v) io.write(">>>>>>",v,"\n") end
     }
-	local function mytrace(pc,tos,code,stack,store) 
-		print(string.format("====[pc=%d]==[tos=%d]====",pc,tos))
-		print("code: ")
-		print(pt.pt(code))
-		print("stack: ")
-		print(pt.pt(stack))
-		print("store: ")
-		print(pt.pt(store))
-	end
+    local function mytrace(pc,tos,code,stack,store) 
+        print(string.format("====[pc=%d]==[tos=%d]====",pc,tos))
+        print("code: ")
+        print(pt.pt(code))
+        print("stack: ")
+        print(pt.pt(stack))
+        print("store: ")
+        print(pt.pt(store))
+    end
     --mytrace = function(...) t={...} print(pt.pt(t[#t-1])) end
-	mytrace = function(...) end	
-	run(code, store, vmio, stack, mytrace)
-	--run(code, store, stack)
+    mytrace = function(...) end 
+    run(code, store, vmio, stack, mytrace)
+    --run(code, store, stack)
     print(pt.pt(stack))
     --]]
 end
